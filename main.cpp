b37f7d553e38daad5242b12ba293b6af323435ee
Glen-Sasek-PCC-Student <glen.sasek+student@pcc.edu>
2025-10-02 20:54:03 +0000

    Working

--- main.cpp
+++ main.cpp
@@ -1,56 +1,74 @@
 // ------------- FILE HEADER -------------
-// Author ✅: 
+// Author ✅:
 // Assignment ✅:
 // Date ✅:
-// Citations: 
-
+// Citations:
 
 // ------------- ZYBOOKS SCORES -------------
-// Chapter ✅: 
-// Participation ✅: 
+// Chapter ✅:
+// Participation ✅:
 // Challenge ✅:
 // Labs ✅:
-
 
 // ------------- DISCORD POSTS -------------
 // https://discord.com/invite/URYKKf8YHm
 // Count ✅:
-// Links (Optional): 
-
+// Links (Optional):
 
 // ------------- DESIGN DOCUMENT -------------
-// A. INPUT ✅: 
+// A. INPUT ✅:
 // B. OUTPUT ✅:
 // C. CALCULATIONS ✅:
 // D. LOGIC and ALGORITHMS ✅:
-//    (Optional) flow chart link or file name: 
-
+//    (Optional) flow chart link or file name:
 
 // ------------- TESTING -------------
-// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
-// (Optional) Additional tests count:   
-
+// PASS ALL GIVEN SAMPLE RUN TESTS ✅:
+// (Optional) Additional tests count:
 
 // ------------- CODE -------------
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 // Function prototypes (if any)
 
+// RUN AND TEST:
+// g++ main.cpp && ./a.out <<< '12' && rm ./a.out
 
 // Main function
 // https://en.cppreference.com/w/cpp/language/main_function.html
-int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
+int main(int argc, char *argv[])
+{
+  int oreos = 0;
+
+  double servings = 0.0;
+  double calories = 0.0;
+
+  const int OREOS_PER_SERVING = 5;
+  const int CALORIES_PER_SERVING = 160;
+  
+  cout << "Welcome to the Oreo Calculator!" << endl;
+  cout << "Enter the number of Oreos eaten: ";
+  cin >> oreos;
+  servings = static_cast<double>(oreos) / OREOS_PER_SERVING;
+  calories = servings * CALORIES_PER_SERVING; 
+  cout << endl;
+
+  cout << fixed << setprecision(1);
+
+  cout << oreos << " Oreos equals " << servings << " servings!" << endl;
+  cout << "You consumed " << calories << " calories." << endl;
+  cout << "Keep eating Oreos!" << endl;
+
   return 0;
 }
 
 // Function implementations (if any)
 
-
 // ------------- DESIGN -------------
-/* 
+/*
 Program Name:
 
 Program Description:
